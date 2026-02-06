96cc110b2ec5b0361ffed087688526b830457d1d
smarni4 <srimarni147@gmail.com>
2026-02-06 00:48:22 -0500

    Author reset

---
+++ README.md
@@ -0,0 +1,7 @@
+**[ETL Testing Guide](https://qaautomation.expert/2025/10/07/a-complete-guide-to-etl-testing/#what-is-etl-extract-transform-load)**
+
+**[Selenium Guide 1](https://www.geeksforgeeks.org/blogs/selenium-project-ideas/)**
+
+**[Selinium Guide 2](https://www.techlistic.com/p/selenium-tutorials.html)**
+
+
--- selenium/first_script.py
+++ selenium/first_script.py
@@ -1,22 +1,38 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
-driver = webdriver.Chrome()
 
-driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+def test_case():
 
-title = driver.title
+    driver = web_setup()
 
-driver.implicitly_wait(0.5)
+    title = driver.title
+    assert title == "Web form"
+    driver.implicitly_wait(0.5)
 
-text_box = driver.find_element(by=By.NAME, value="my-text")
-submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
+    text_box = driver.find_element(by=By.NAME, value="my-text")
+    submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
 
-text_box.send_keys("Selenium")
-submit_button.click()
+    text_box.send_keys("Selenium")
+    time.sleep(10)
+    submit_button.click()
 
-message = driver.find_element(by=By.ID, value="message")
-text = message.text
+    message = driver.find_element(by=By.ID, value="message")
+    text = message.text
+    assert text == "Received!"
+    
+    teardown(driver)
 
-driver.quit()
+
+def web_setup():
+
+    driver = webdriver.Chrome()
+    driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+    return driver
+
+
+def teardown(driver):
+    
+    driver.quit()
 
